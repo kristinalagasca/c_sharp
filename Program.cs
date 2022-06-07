cac353434b7ad8b382858a5600d7f7172bf3b274
Kristina Lagasca <kristinalagasca@gmail.com>
2022-06-06 22:15:09 -0400

    Updating C# learnings

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace TestNamespace
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tina";
+            Console.WriteLine("Hi! My name is " + string.Join(" ", name));
+
+            int favoriteNumber = 13;
+            Console.WriteLine("My favorite number is " + string.Join(" ", favoriteNumber));
+
+            bool myBoolean = true;
+            Console.WriteLine("My boolean value is " + string.Join(" ", myBoolean));
+
+            double myDouble = 0.9;
+            Console.WriteLine("My double value is " + string.Join(" ",myDouble));
+
+            // char myChar = "K";
+            // Console.WriteLine("My char value is " + string.Join(" ", myChar));
+        }
+    }
+}
